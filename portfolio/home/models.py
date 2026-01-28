c7588b8b56e836c0d9bbcc6e4b68d917d002f0af
umarbashir-dotcom <umarbasheer449@gmail.com>
2026-01-28 09:26:03 +0500

    footer added

--- portfolio/home/models.py
+++ portfolio/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    subject = models.CharField(max_length=500)
+    email = models.CharField(max_length=50)
+    message = models.CharField(max_length=500)
--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -78,12 +78,24 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Contacts',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
